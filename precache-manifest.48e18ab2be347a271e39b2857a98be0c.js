93595bdf0d9335c25e10607f4e8f11cdcc9e410c
Amit Mishra <amit.aakash@gmail.com>
2020-01-24 00:04:44 +0530

    Burger app

---
+++ precache-manifest.48e18ab2be347a271e39b2857a98be0c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f06a264aceac586adbfa4fd024ca128",
+    "url": "/index.html"
+  },
+  {
+    "revision": "02035513a3b2ae412a54",
+    "url": "/static/css/main.91e512af.chunk.css"
+  },
+  {
+    "revision": "03c70a1f2c3d307f99bc",
+    "url": "/static/js/2.33203979.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.33203979.chunk.js.LICENSE"
+  },
+  {
+    "revision": "02035513a3b2ae412a54",
+    "url": "/static/js/main.1e62ae31.chunk.js"
+  },
+  {
+    "revision": "172116f184c4321f02df",
+    "url": "/static/js/runtime-main.76bbe192.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/static/media/burger-logo.b8503d26.png"
+  }
+]);
